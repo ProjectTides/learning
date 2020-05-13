39751172fc782729be4c655aed897b85866bb251
pankaj-ag <pankaj@coloredcow.in>
2020-05-13 17:47:29 +0530

    excerises

--- practice.exs
+++ practice.exs
@@ -25,4 +25,24 @@ defmodule WordCount do
     end
 end
 
-printLog.(WordCount.count("Hello world Hello"))
+str = String.replace(List.to_string('GC'), ["G", "C"], fn <<char>> 
+          -> 
+           map = %{"G" => "C", "C" => "G", "T" => "A", "A" => "U"}
+           map[<<char>>] 
+      end)
+
+printLog.(str)
+
+
+
+# printLog = fn text -> IO.puts "printing the #{text} \n ............ " end
+
+#str = String.replace('G', ['G'], fn [head | _tail] -> [head + 1] end)
+
+# printLog.(String.to_charlist("G"))
+
+# list =  List.replace_at('G', 0, 0)
+# ll = List.to_string('G')
+
+# printLog.(ll)
+# IO.inspect(list, charlists: :as_lists)
