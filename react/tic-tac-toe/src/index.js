2772c86330ec9d50087fcc82b8a68d34e0c297e0
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-05-16 19:53:04 +0530

    rendering grid with two arrays

--- react/tic-tac-toe/src/index.js
+++ react/tic-tac-toe/src/index.js
@@ -14,31 +14,21 @@ class Board extends React.Component {
     renderSquare(i) {
         return (
             <Square
+                key={i}
                 value={this.props.squares[i]}
                 onClick={() => this.props.onClick(i)}
             />
         );
     }
     render() {
-        return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+        let grid = [...Array(3).keys()].map((row) => (
+            <div key={row} className="board-row">
+                {[...Array(3).keys()].map((col) => (
+                    this.renderSquare(row * 3 + col)
+                ))}
             </div>
-        );
+        ));
+        return (<div>{grid}</div>);
     }
 }
 
