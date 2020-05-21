cea660be1db6bd84abf9fe8307dc417fd9264d84
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-05-22 00:31:34 +0530

    all things cleaned

--- react/book-keeper-new/src/App.js
+++ react/book-keeper-new/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import styles from "./App.module.css";
 import BookList from "./containers/BookList";
 
 function App() {
   return (
-    <div className="app">
-      <h1 className="page-title">Book Keeper</h1>
+    <div className={styles.App}>
+      <h1 className={styles.PageTitle}>Book Keeper</h1>
       <BookList />
     </div>
   );
